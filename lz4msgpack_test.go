29d904a71353badc5a14078eefe017c951ddebf8
honda.tatsuya <honda.tatsuya@donuts.ne.jp>
2018-10-04 11:27:56 +0900

    add test

---
+++ lz4msgpack_test.go
@@ -0,0 +1,39 @@
+package lz4msgpack
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/vmihailenco/msgpack"
+
+	"github.com/d-o-n-u-t-s/lz4msgpack"
+)
+
+func Test(t *testing.T) {
+	type Data struct {
+		A int
+		B float32
+		C []string
+	}
+
+	data := Data{
+		A: 4578234323,
+		B: 1.46437485,
+		C: []string{"Hello World", "Hello World", "Hello World", "Hello World", "Hello World"},
+	}
+	t.Log(data)
+
+	tester := func(name string, marshaler func(v ...interface{}) ([]byte, error)) {
+		b, _ := marshaler(&data)
+		t.Logf("%s: %d", name, len(b))
+		var data1 Data
+		lz4msgpack.Unmarshal(b, &data1)
+		if !reflect.DeepEqual(data, data1) {
+			t.Fatal(name + " Error")
+		}
+	}
+
+	tester("          msgpack.Marshal", msgpack.Marshal)
+	tester("       lz4msgpack.Marshal", lz4msgpack.Marshal)
+	tester("lz4msgpack.MarshalAsArray", lz4msgpack.MarshalAsArray)
+}
