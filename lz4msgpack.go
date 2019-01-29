7964fd008376ee35268c05aa47a4f0c0dd2ae95e
ito.takaaki <ito.takaaki@donuts.ne.jp>
2019-01-29 12:50:07 +0900

    共通処理を切り出し

--- lz4msgpack.go
+++ lz4msgpack.go
@@ -22,16 +22,16 @@ const (
 
 // Marshal returns bytes that is the MessagePack encoded and lz4 compressed.
 func Marshal(v interface{}) ([]byte, error) {
-	data, err := msgpack.Encode(v)
-	if err != nil {
-		return data, err
-	}
-	return compress(data)
+	return marshal(v, msgpack.Encode)
 }
 
-// MarshalAsArray returns bytes as array format.
+// MarshalAsArray returns bytes as array format that is the MessagePack encoded and lz4 compressed.
 func MarshalAsArray(v interface{}) ([]byte, error) {
-	data, err := msgpack.EncodeStructAsArray(v)
+	return marshal(v, msgpack.EncodeStructAsArray)
+}
+
+func marshal(v interface{}, marshaler func(interface{}) ([]byte, error)) ([]byte, error) {
+	data, err := marshaler(v)
 	if err != nil {
 		return data, err
 	}
@@ -59,28 +59,24 @@ func compress(data []byte) ([]byte, error) {
 // in the value pointed to by v.
 // In case of data compressed by lz4, it will be uncompressed before decode.
 func Unmarshal(data []byte, v interface{}) error {
-	if data[offsetCodeExt32] != msgpackCodeExt32 || data[offsetCodeLz4] != extCodeLz4 {
-		return msgpack.Decode(data, v)
-	}
-	buf := make([]byte, binary.BigEndian.Uint32(data[offsetUncompressSize:offsetLength]))
-	_, err := lz4.UncompressBlock(data[offsetLength:], buf)
-	if err != nil {
-		return err
-	}
-	return msgpack.Decode(buf, v)
+	return unmarshal(data, v, msgpack.Decode)
 }
 
 // UnmarshalAsArray decodes the array format MessagePack-encoded data and stores the result
 // in the value pointed to by v.
 // In case of data compressed by lz4, it will be uncompressed before decode.
 func UnmarshalAsArray(data []byte, v interface{}) error {
+	return unmarshal(data, v, msgpack.DecodeStructAsArray)
+}
+
+func unmarshal(data []byte, v interface{}, unmarshaler func([]byte, interface{}) error) error {
 	if data[offsetCodeExt32] != msgpackCodeExt32 || data[offsetCodeLz4] != extCodeLz4 {
-		return msgpack.DecodeStructAsArray(data, v)
+		return unmarshaler(data, v)
 	}
 	buf := make([]byte, binary.BigEndian.Uint32(data[offsetUncompressSize:offsetLength]))
 	_, err := lz4.UncompressBlock(data[offsetLength:], buf)
 	if err != nil {
 		return err
 	}
-	return msgpack.DecodeStructAsArray(buf, v)
+	return unmarshaler(buf, v)
 }
